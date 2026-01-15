3f692fd3c0b1a8c912c13b47c53eed898f436569
Carter Zielinski <zielinca@bc.edu>
2026-01-15 18:58:38 -0500

    Finished Lesson 1.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageName = ""
+    @State private var imageString = ""
     
     var body: some View {
         
         VStack {
             Spacer()
             
-            Image(systemName: imageName)
+            Image(systemName: imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
@@ -31,17 +31,20 @@ struct ContentView: View {
             Button("Press Me!") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-                let imageString1 = "sun.max.fill"
-                let imageString2 = "hand.thumbsup"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
                 
                 
-                if message == message1{
-                    message = message2
-                    imageName = imageString2
-                } else {
-                    message = message1
-                    imageName = imageString1
-                }
+//                if message == message1{
+//                    message = message2
+//                    imageName = imageString2
+//                } else {
+//                    message = message1
+//                    imageName = imageString1
+//                }
+                
+                message = ( message == message1 ? message2 : message1 )
+                imageString = ( imageString == imageString1 ? imageString2 : imageString1 )
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
