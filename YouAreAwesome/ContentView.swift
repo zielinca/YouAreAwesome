be05d830e1afda19da83523c2ba6b90f1c8fcdeb
Carter Zielinski <zielinca@bc.edu>
2026-01-16 09:24:21 -0500

    Lesson 1.8 Complete

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,46 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageString = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
     
     var body: some View {
         
         VStack {
             Spacer()
             
-            Image(systemName: imageString)
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
-                .foregroundStyle(.black)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
             
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
-                
-                
-//                if message == message1{
-//                    message = message2
-//                    imageName = imageString2
-//                } else {
-//                    message = message1
-//                    imageName = imageString1
-//                }
                 
                 message = ( message == message1 ? message2 : message1 )
-                imageString = ( imageString == imageString1 ? imageString2 : imageString1 )
+                imageName = "image\(imageNumber)"
+//                imageName = ( imageName == "image0" ? "image1" : "image0" )
+//                imageNumber = imageNumber + 1
+                imageNumber += 1
+                
+                if imageNumber > 9{
+                    imageNumber = 0
+                }
+                print(imageNumber)
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
             
         }
         .padding()
