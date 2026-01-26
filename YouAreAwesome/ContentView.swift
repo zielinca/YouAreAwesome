712b85bbad06e919a717346012d0f84b839b67c0
Carter Zielinski <zielinca@bc.edu>
2026-01-26 11:42:25 -0500

    End of Lessons 1.9 and 1.10

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,38 +11,51 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
+    @State private var messageNumber = 0
     
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .animation(.default, value: imageName)
             
             Spacer()
             
             Button("Show Message") {
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
+
+                let messages = ["You Are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
+                                "You Are Great!",
+                                "You Are Fantastic!",
+                                "Fabulous? That's You!",
+                                "You Make Me Smile!"
+                                ]
                 
-                message = ( message == message1 ? message2 : message1 )
+                message = messages[messageNumber]
+                messageNumber += 1
+                if messageNumber == messages.count{
+                    messageNumber = 0
+                }
+                    
                 imageName = "image\(imageNumber)"
                 imageNumber += 1
                 
                 if imageNumber > 9{
                     imageNumber = 0
                 }
-                print(imageNumber)
-                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
